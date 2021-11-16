c74d5adcc16b46440d11ade7c66d56ae47b2d5af
DominicPuzzuoli <puzzuoli@oakland.edu>
2021-11-16 18:34:23 -0500

    database

--- config/dbcon.php
+++ config/dbcon.php
@@ -4,6 +4,10 @@ require __DIR__.'/vendor/autoload.php';
 
 use Kreait\Firebase\Factory;
 
-$factory = (new Factory)->withServiceAccount('/path/to/firebase_credentials.json');
+$factory = (new Factory)
+    ->withServiceAccount('nfcurls-firebase-adminsdk-m6gq5-38b9cf9a45')
+    ->withDatabaseUri('https://nfcurls-default-rtdb.firebaseio.com/');
 
+    $database = $factory->createDatabase();
+    
 ?>
