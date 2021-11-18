a333eb28c2bb33638dfbe696d4f886696f3477dc
DominicPuzzuoli <puzzuoli@oakland.edu>
2021-11-17 19:03:12 -0500

    lots of changes

---
+++ public/script.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js'
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js'
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.4.1/firebase-database.js'
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBYvjl_09gdM8dq1Uh__qjAERKDcPsyOAk",
+  authDomain: "nfcurls.firebaseapp.com",
+  databaseURL: "https://nfcurls-default-rtdb.firebaseio.com",
+  projectId: "nfcurls",
+  storageBucket: "nfcurls.appspot.com",
+  messagingSenderId: "48180444980",
+  appId: "1:48180444980:web:682281cf5debcb7811ef61",
+  measurementId: "G-T2CF60NQPB"
+});
+
+const auth = getAuth(firebaseApp);
+const db = getDatabase(firebaseApp);
