f37cdd18586cc154ef3368d9b77d3c132bffe718
b3nlumsden <71798993+b3nlumsden@users.noreply.github.com>
2021-11-18 20:17:14 -0500

    Can write and make new documents in user

--- public/scripts/onSignIn.js
+++ public/scripts/onSignIn.js
@@ -1,3 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
+import { getFirestore, doc, getDoc, getDocs, collection, setDoc} from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYvjl_09gdM8dq1Uh__qjAERKDcPsyOAk",
+  authDomain: "nfcurls.firebaseapp.com",
+  databaseURL: "https://nfcurls-default-rtdb.firebaseio.com",
+  projectId: "nfcurls",
+  storageBucket: "nfcurls.appspot.com",
+  messagingSenderId: "48180444980",
+  appId: "1:48180444980:web:682281cf5debcb7811ef61",
+  measurementId: "G-T2CF60NQPB"
+};
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBYvjl_09gdM8dq1Uh__qjAERKDcPsyOAk",
+  authDomain: "nfcurls.firebaseapp.com",
+  databaseURL: "https://nfcurls-default-rtdb.firebaseio.com",
+  projectId: "nfcurls",
+  storageBucket: "nfcurls.appspot.com",
+  messagingSenderId: "48180444980",
+  appId: "1:48180444980:web:682281cf5debcb7811ef61",
+  measurementId: "G-T2CF60NQPB"
+});
+const app = initializeApp(firebaseConfig);
+
 function onSignIn(googleUser)
 {
     var profile=googleUser.getBasicProfile();
@@ -5,6 +31,7 @@ function onSignIn(googleUser)
     $(".data").css("display","block");
     $("#pic").attr('src',profile.getImageUrl());
     $("#email").text(profile.getEmail());
+
 }
 
 function signOut()
@@ -18,3 +45,12 @@ function signOut()
         $(".data").css("display","none");
     })
 }
+
+const db = getFirestore(app);
+const userRef = collection(db, "users")
+
+await setDoc(doc(userRef, "SA"), {
+    name: "San Francisco", state: "CA", country: "USA",
+    capital: false, population: 860000,
+    regions: ["west_coast", "norcal"] });
+alert("alert");
--- public/scripts/writeData.js
+++ public/scripts/writeData.js
@@ -3,7 +3,6 @@ import { getFirestore, doc, getDoc, getDocs, collection, setDoc} from "https://w
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBYvjl_09gdM8dq1Uh__qjAERKDcPsyOAk",
   authDomain: "nfcurls.firebaseapp.com",
@@ -19,32 +18,10 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const docRef = doc(db, "users", "uid");
-const docSnap = await getDoc(docRef);
 
-if (docSnap.exists()) {
-  console.log("Document data:", docSnap.data());
-} else {
-  // doc.data() will be undefined in this case
-  console.log("No such document!");
-}
+
 const citiesRef = collection(db, "cities");
 await setDoc(doc(citiesRef, "SF"), {
     name: "San Francisco", state: "CA", country: "USA",
     capital: false, population: 860000,
     regions: ["west_coast", "norcal"] });
-await setDoc(doc(citiesRef, "LA"), {
-    name: "Los Angeles", state: "CA", country: "USA",
-    capital: false, population: 3900000,
-    regions: ["west_coast", "socal"] });
-await setDoc(doc(citiesRef, "DC"), {
-    name: "Washington, D.C.", state: null, country: "USA",
-    capital: true, population: 680000,
-    regions: ["east_coast"] });
-await setDoc(doc(citiesRef, "TOK"), {
-    name: "Tokyo", state: null, country: "Japan",
-    capital: true, population: 9000000,
-    regions: ["kanto", "honshu"] });
-await setDoc(doc(citiesRef, "BJ"), {
-    name: "Beijing", state: null, country: "China",
-    capital: true, population: 21500000,
-    regions: ["jingjinji", "hebei"] });
