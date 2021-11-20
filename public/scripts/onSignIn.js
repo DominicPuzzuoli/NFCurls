09863e1c412411a2b2c6df17ac5d4d4d2d32fc30
DominicPuzzuoli <puzzuoli@oakland.edu>
2021-11-20 13:47:11 -0500

    Added nav bar and css

--- public/scripts/onSignIn.js
+++ public/scripts/onSignIn.js
@@ -2,9 +2,9 @@ function onSignIn(googleUser)
 {
     var profile=googleUser.getBasicProfile();
     $(".g-signin2").css("display","none");
-    $(".data").css("display","block");
+    $(".data").css("display","inline-block");
     $("#pic").attr('src',profile.getImageUrl());
-    $("#email").text(profile.getEmail());
+    
 }
 
 function signOut()
