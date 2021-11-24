5cbdc85de37111360bccb5a55e3e2845155118b0
DominicPuzzuoli <puzzuoli@oakland.edu>
2021-11-23 19:18:48 -0500

    google auth

---
+++ public/auth_google_checksameuser.js
@@ -0,0 +1,15 @@
+import { GoogleAuthProvider } from "firebase/auth";
+
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    const providerData = firebaseUser.providerData;
+    for (let i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
